b08376847ae782fccdecf10dfe1edd09770dda2c
duyktk <duyktkse161141@fpt.edu.vn>
2022-07-21 20:27:42 +0700

    Update ProductDAO.cs

--- DataAccess/ProductDAO.cs
+++ DataAccess/ProductDAO.cs
@@ -10,6 +10,7 @@ using BusinessObject;
 using System.IO;
 using Microsoft.Extensions.Configuration;
 using BusinessObject.Models;
+using DataAccess.Repository;
 
 namespace DataAccess
 {
@@ -37,18 +38,18 @@ namespace DataAccess
         }
         public List<Product> ProductList = new List<Product>();
 
-        public IEnumerable<Product> GetMemberList()
+        public IEnumerable<Product> GetProductList()
         {
             IDataReader dataReader = null;
             string SQLSelect = "Select ProductID, CategoryID, ProductName, Weight, UnitPrice, UnitslnStock " +
                 "From Product";
-            var members = new List<Product>();
+            var products = new List<Product>();
             try
             {
                 dataReader = dataProvider.GetDataReader(SQLSelect, CommandType.Text, out connection);
                 while (dataReader.Read())
                 {
-                    members.Add(new Product
+                    products.Add(new Product
                     {
                         ProductId = dataReader.GetInt32(0),
                         CategoryId = dataReader.GetInt32(1),
@@ -68,7 +69,7 @@ namespace DataAccess
                 dataReader.Close();
                 CloseConnection();
             }
-            return members;
+            return products;
         }
         public Product GetProductByName(string productName)
         {
@@ -114,9 +115,9 @@ namespace DataAccess
                 if (addPro == null)
                 {
                     string SQLInsert = "Insert Product " +
-                        "values(@CategoryId,@ProductName,@Weight,@UnitPrice,@UnitslnStock)";
+                        "values(@ProductId,@ProductName,@Weight,@UnitPrice,@UnitslnStock)";
                     var parameters = new List<SqlParameter>();
-                    parameters.Add(dataProvider.CreateParameter("@CategoryId", 100, product.CategoryId, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@ProductId", 100, product.ProductId, DbType.String));
                     parameters.Add(dataProvider.CreateParameter("@ProductName", 40, product.ProductName, DbType.String));
                     parameters.Add(dataProvider.CreateParameter("@Weight", 15, product.Weight, DbType.String));
                     parameters.Add(dataProvider.CreateParameter("@UnitPrice", 15, product.UnitPrice, DbType.String));
@@ -147,14 +148,14 @@ namespace DataAccess
                 if (proUpdate != null)
                 {
                     string SQLUpdate = "Update Product" +
-                        " set CategoryId = @CategoryId," +
+                        " set ProductId = @ProductId," +
                             " ProductName = @ProductName" +
                             " Weight = @Weight" +
                             " UnitPrice = @UnitPrice" +
                             " UnitslnStock = @UnitslnStock" +
                         " Where ProductName = @ProductName";
                     var parameters = new List<SqlParameter>();
-                    parameters.Add(dataProvider.CreateParameter("@CategoryId", 100, product.CategoryId, DbType.String));
+                    parameters.Add(dataProvider.CreateParameter("@ProductId", 100, product.ProductId, DbType.String));
                     parameters.Add(dataProvider.CreateParameter("@ProductName", 40, product.ProductName, DbType.String));
                     parameters.Add(dataProvider.CreateParameter("@Weight", 15, product.Weight, DbType.String));
                     parameters.Add(dataProvider.CreateParameter("@UnitPrice", 15, product.UnitPrice, DbType.String));
