a5261a7e3815025a8654c1d58fd406568b5769dd
duyktk <duyktkse161141@fpt.edu.vn>
2022-07-22 20:04:33 +0700

    Update ProductRepository.cs

--- DataAccess/Repository/ProductRepository.cs
+++ DataAccess/Repository/ProductRepository.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObject;
+using BusinessObject.Models;
 
 namespace DataAccess.Repository
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        public void AddNewProduct(Product product)
+             => ProductDAO.Instace.AddNew(product);
+
+        public Product GetProductByName(string productName)
+            => ProductDAO.Instace.GetProductByName(productName);
+
+        public IEnumerable<Product> GetProductsList()
+            => ProductDAO.Instace.GetProductList();
+
+        public void RemoveProduct(String productName)
+            => ProductDAO.Instace.Remove(productName);
+
+        public void UpdateProduct(Product product)
+        => ProductDAO.Instace.Update(product);
     }
 }
